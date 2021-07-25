2846e668f9e2e59d8dcf4ce09dd58d5f5a4cddf3
Vipul Tanwar <vipultanwar45@gmail.com>
2021-07-25 17:23:37 +0530

    Task Complete

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-// console.log(collegesObject);
 const collegesDeck = document.getElementById("deck")
 let collegeTemplates = ``;
 let loadCollage = 10;
-let temp = 0;
+let newLoad = 0;
 
 function loadCollages(){
-for( var i = temp; i < loadCollage; i++){
+for( var i = newLoad; i < loadCollage; i++){
     let collegeInfo = collegesObj.colleges[i];
-    // console.log(collegeInfo);
-    let { image } = collegeInfo
+    
     collegeTemplates += `
     <div class="card">
                 <div class="ribbon">
                     <p>PROMOTED</p>
                 </div>
 
-                <div class="card-image" style="position: relative;">   
-                    <img src="images/${image}"  alt="">
+                <div class="card-image">   
+                    <img src="images/${collegeInfo.image}"  alt="">
                     <div class="img-filter" ></div>
 
                     <div class="img-details">
@@ -38,7 +36,7 @@ for( var i = temp; i < loadCollage; i++){
                     <div class="college-fees-details" >
                         <div class="college-fees">
                         
-                            <p>₹${collegeInfo.original_fees}</p>
+                            <p>₹${collegeInfo.original_fees.toLocaleString() }</p>
 
                             <div class="disc-tag" >
                                 <div class="tri"></div>
@@ -48,12 +46,12 @@ for( var i = temp; i < loadCollage; i++){
 
                         </div>
                         
-                        <p class="disk-fees">₹${collegeInfo.discounted_fees}</p>
+                        <p class="disk-fees">₹${collegeInfo.discounted_fees.toLocaleString() }</p>
                         <p class="sem-fees">${collegeInfo.fees_cycle}</p>
                     </div>
                     <div class="college-name">
                         <h3 class="college-title">${collegeInfo.college_name} 
-                        <span class="first-span">
+                        <span class="star-span">
                             <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" data-name="Layer 1" viewBox="0 0 122.88 117.1"><defs><style>.cls-1{fill:#565656;;}</style></defs><title>star-symbol</title><path class="cls-1" d="M64.42,2,80.13,38.7,120,42.26a3.2,3.2,0,0,1,1.82,5.62h0L91.64,74.18l8.9,39A3.19,3.19,0,0,1,98.12,117a3.27,3.27,0,0,1-2.46-.46L61.41,96.1,27.07,116.64a3.18,3.18,0,0,1-4.38-1.09,3.14,3.14,0,0,1-.37-2.38h0l8.91-39L1.09,47.88a3.24,3.24,0,0,1-.32-4.52,3.32,3.32,0,0,1,2.29-1l39.72-3.56L58.49,2a3.24,3.24,0,0,1,5.93,0Z"/></svg>
                         </span>
                         <span>
@@ -82,12 +80,15 @@ for( var i = temp; i < loadCollage; i++){
 }
 collegesDeck.innerHTML = collegeTemplates;
 loadCollage += 10;
-temp += 10;
+newLoad += 10;
+console.log(newLoad)
 }
 
 var intersectionObserver = new IntersectionObserver(function(entries) {
     if (entries[0].intersectionRatio <= 0) return;
-    loadCollages();
+    if( loadCollage <=  collegesObj.colleges.length){
+        loadCollages();
+    }
 });
 
 intersectionObserver.observe(document.querySelector('#last-element'));
